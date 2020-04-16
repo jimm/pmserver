ddf67c70d341f7a0e0eb4e8318be9890e0d22cd2
Jim Menard <jim.menard@warbyparker.com>
2020-04-16 17:16:54 -0400

    Switch to using Catch2 for testing

--- test/pmserver_test.cpp
+++ test/pmserver_test.cpp
@@ -5,44 +5,12 @@
 #include "pmserver_test.h"
 #include "../src/server.h"
 
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#define CATCH_CATEGORY "[hex]"
+
 using std::vector;
-
-// ================ running tests ================
-
-test_results results;
-
-void test_results_init() {
-  results.num_tests = results.num_errors = 0;
-}
-
-void test_passed() {
-  ++results.num_tests;
-  printf(".");
-}
-
-void test_failed() {
-  ++results.num_tests;
-  ++results.num_errors;
-  printf("*");
-}
-
-int test_num_errors() {
-  return results.num_errors;
-}
-
-void print_time() {
-  rusage usage;
-  getrusage(RUSAGE_SELF, &usage);
-  printf("\n\nFinished in %ld.%06ld seconds\n", usage.ru_utime.tv_sec,
-         (long)usage.ru_utime.tv_usec);
-}
-
-void print_results() {
-  printf("\nTests run: %d, tests passed: %d, tests failed: %d\n",
-         results.num_tests, results.num_tests - results.num_errors,
-         results.num_errors);
-  printf("done\n");
-}
 
 // ================ pmserver tests ================
 
@@ -56,20 +24,15 @@ void hex_word_test(const char * const str, byte expected[], int num_expected) {
   char errmsg[BUFSIZ];
 
   server.hex_word_to_bytes(str, bytes);
-
-  sprintf(errmsg, "%s converted length %d is wrong, expected %d\n",
-          str, (int)bytes.size(), num_expected);
-  tassert(bytes.size() == num_expected, errmsg);
+  REQUIRE(bytes.size() == num_expected);
 
   for (int i = 0; i < num_expected; ++i) {
     byte seen = bytes.at(i);
-    sprintf(errmsg, "%s index %d bad byte 0x%02x seen, expected 0x%02x\n",
-            str, i, seen, expected[i]);
-    tassert(seen == expected[i], errmsg);
+    REQUIRE(seen == expected[i]);
   }
 }
 
-void test_hex_word_to_bytes() {
+TEST_CASE("hex word to bytes", CATCH_CATEGORY) {
   byte bytes[BUFSIZ];
 
   memset(bytes, 0, BUFSIZ);
@@ -94,22 +57,4 @@ void test_hex_word_to_bytes() {
 
   fprintf(stderr, "expect to see an error message here about odd # digits\n");
   hex_word_test("1234a", bytes, 2);
-}
-
-void run_tests() {
-  test_hex_word_to_bytes();
-}
-
-void run_tests_and_print_results() {
-  run_tests();
-  print_time();
-  print_results();
-}
-
-// ================ main ================
-
-int main(int argc, const char **argv) {
-  run_tests_and_print_results();
-  exit(results.num_errors == 0 ? 0 : 1);
-  return 0;
 }
